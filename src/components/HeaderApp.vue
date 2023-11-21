64b2da64552e706db555c7aaab8fe2046027a280
chiara1911 <chiaracesari89@gmail.com>
2023-11-21 12:55:07 +0100

    add image and top-header

--- src/components/HeaderApp.vue
+++ src/components/HeaderApp.vue
@@ -1,7 +1,32 @@
 <template >
-  <div class="ms-2">
-    <h1 >Header</h1>
-    <div class="top-bar bg-danger">topbar</div>
+  <div class="ms-2 bg-danger">    
+    <h1 class="text-center ">header</h1>
+    <div class="container">
+    <div class="top-bar  mt-2 d-flex flex-row justify-content-between ">
+      <div id="top-left">
+        <button class="btn bg-secondary text-uppercase text-light"> news updates</button>
+      <span class="text-uppercase">05:35 the_</span>
+      </div>
+      <div class="top-right d-flex flex-row justify-content-between align-content-between mx-1 text-light">
+        <span class="icon-left mx-4">
+          <i class="fa-solid fa-angle-left "></i>
+        <i class="fa-solid fa-angle-right"></i>
+        </span>
+        
+          <ul class="d-flex flex-row list-unstyled icon-right">
+            <li><i class="fa-brands fa-facebook"></i></li>
+            <li><i class="fa-brands fa-twitter"></i></li>
+            <li><i class="fa-brands fa-instagram"></i></li>
+            <li><i class="fa-brands fa-youtube"></i></li>
+          </ul>
+        </div>
+      
+      
+       
+
+      </div>
+      
+    </div>
     <div class="container bg-info "> container 
       <div>loghi</div>
       <div>lista</div>
@@ -20,4 +45,7 @@
 
 <style lang="scss" scoped>
 
+i{
+  padding-left: 5px;
+}
 </style>
